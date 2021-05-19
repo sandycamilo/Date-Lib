a86fe666c979dfaf5a2d86db740fe343bb272d31
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-19 16:05:12 -0700

    update

--- src/index.js
+++ src/index.js
@@ -222,33 +222,35 @@ var D = /** @class */ (function () {
     };
     return D;
 }());
+
 module.exports = D;
-// const d = new D() 
+
+const d = new D() 
 // Externally year looks like a property! - No parantheses
-// console.log( d.year )  // 2021 - Full year
-// console.log( d.yr )    // 21   - Short year
-// console.log( d.month ) // July - Full month
-// console.log( d.mon )   // Jul  - Short month
-// console.log( d.day )   // Tuesday - Full day
-// console.log( d.dy )    // Tue  - Short day
-// console.log( d.date )  // 27   - Date
-// console.log( d.hour ) // 18   - Hour
-// console.log( d.mins )  // 6    - Minutes
-// console.log( d.seconds, '<- seconds')  // 5    - Seconds
-// const d = new D(2017, 0, 2, 3, 4, 5)
-// console.log(d.format()) // 2017 January 02
-// console.log(d.format('y/m/d')) // 17/Jan/2
-// console.log(d.format('H:I:S')) // 03:04:05
-// console.log(d.format('h:i:s')) // 3:4:5
-// console.log(d.format('Y-M-D h:I:S')) // 2017-January-02 3:04:05
-// console.log(d.format('W:w')) 
-// const d = new D(2019, 0, 2, 3, 4, 5)
-// console.log(d.when()) // 6 months ago
-// const d = new D(2019, 9, 2, 3, 4, 5)
-// console.log(d.when()) // 3 months from now
-// const d = new D(2024, 9, 2, 3, 4, 5)
-// console.log(d.when()) // 5 years from now
-// const d = new D(2019, 6, 30, 3, 4, 5)
-// console.log(d.when()) // 3 days from now
-// const d = new D()
-// console.log(d.when()) // today
+console.log( d.year )  // 2021 - Full year
+console.log( d.yr )    // 21   - Short year
+console.log( d.month ) // July - Full month
+console.log( d.mon )   // Jul  - Short month
+console.log( d.day )   // Tuesday - Full day
+console.log( d.dy )    // Tue  - Short day
+console.log( d.date )  // 27   - Date
+console.log( d.hour ) // 18   - Hour
+console.log( d.mins )  // 6    - Minutes
+console.log( d.seconds, '<- seconds')  // 5    - Seconds
+const e = new D(2017, 0, 2, 3, 4, 5)
+console.log(e.format()) // 2017 January 01
+console.log(e.format('y/m/d')) // 17/Jan/2
+console.log(e.format('H:I:S')) // 03:04:05
+console.log(e.format('h:i:s')) // 3:4:5
+console.log(e.format('Y-M-D h:I:S')) // 2017-January-02 3:04:05
+console.log(e.format('W: w')) 
+const f = new D(2019, 0, 2, 3, 4, 5)
+console.log(f.when()) 
+const g = new D(2019, 9, 2, 3, 4, 5)
+console.log(g.when()) 
+const h = new D(2044, 9, 2, 3, 4, 5)
+console.log(h.when())
+const k = new D(2020, 6, 30, 3, 4, 5)
+console.log(k.when()) 
+const l = new D()
+console.log(l.when()) // today
